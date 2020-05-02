a930e8ba14fa789b8281356b2b05ca683220e9cc
ajujacob <ajugjacob@gmail.com>
2020-05-02 21:26:31 +0530

    Update 2

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'title' : 'Blog post 1',
+        'name' : 'Aju Jacob',
+        'date' : '2nd May',
+        'content' : 'Hi there, this is my first post'
+    },
+    {
+        'title' : 'Blog post 2',
+        'name' : 'Jane Doe',
+        'date' : '3nd May',
+        'content' : 'Hello there, this is my second post'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 
